3277072fb287905d7157267ec1ece78940c02d66
Morgane Riviere <morgane.riviere@gmail.com>
2016-12-02 08:25:24 +0100

    no message

--- Source/app/src/main/java/start/morgane/holidayspecial/Movie.java
+++ Source/app/src/main/java/start/morgane/holidayspecial/Movie.java
@@ -1,6 +1,6 @@
 package start.morgane.holidayspecial;
 import java.util.List;
-
+import java.util.Date;
 /**
  * Created by Morgane on 26/11/16.
  */
@@ -11,6 +11,8 @@ public class Movie {
     public String synopsis;
     public String imgID;
 
+    public Date releaseDate;
+    public Date watchDate;
 
     public Movie(String title){
         name = title;
